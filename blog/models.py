90bda67543d974c5f44b29c8252e8443fbe63157
BrightMoonMeng <rladudgnrlad@gmail.com>
2022-10-26 14:17:58 +0900

    blog.Post 모델 및 migration 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+
+# post mode
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    
